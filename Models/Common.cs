283fab17055b9f6def1ba4bd407ad1ec8920f72b
east71st <east71st@outlook.com>
2025-12-03 16:49:03 +0900

    修正：定数の定義をreadonlyに訂正

--- Models/Common.cs
+++ Models/Common.cs
@@ -30,42 +30,42 @@ namespace KakeiboForMVC.Models
         /// <summary>
         /// 表示画面の家計簿テーブル最大表示件数
         /// </summary>
-        public static int MaxDisplayKakeiboList { get; set; } = 1000;
+        public static readonly int MaxDisplayKakeiboList = 1000;
 
         /// <summary>
         /// 年月日表示用書式"yyyy年MM月dd日"
         /// </summary>
-        public static string DisplayDateFormat { get; set; } = "yyyy年MM月dd日";
+        public static readonly string DisplayDateFormat = "yyyy年MM月dd日";
 
         /// <summary>
         /// 年月日略式表示用書式"yyyy/MM/dd"
         /// </summary>
-        public static string DisplayShortDateFormat { get; set; } = "yyyy/MM/dd";
+        public static readonly string DisplayShortDateFormat = "yyyy/MM/dd";
 
         /// <summary>
         /// 年月表示用書式"yyyy年MM月"
         /// </summary>
-        public static string DisplayMonthFormat { get; set; } = "yyyy年MM月";
+        public static readonly string DisplayMonthFormat = "yyyy年MM月";
 
         /// <summary>
         /// 年月日データ用書式"yyyy-MM-dd"
         /// </summary>
-        public static string DataDateFormat { get; set; } = "yyyy-MM-dd";
+        public static readonly string DataDateFormat = "yyyy-MM-dd";
 
         /// <summary>
         /// 年月日略式データ用書式"yyyyMMdd"
         /// </summary>
-        public static string DataDateShortFormat { get; set; } = "yyyyMMdd";
+        public static readonly string DataDateShortFormat = "yyyyMMdd";
 
         /// <summary>
         /// 数値表示用書式"###,###"
         /// </summary>
-        public static string DisplayNumberFormat { get; set; } = "###,###";
+        public static readonly string DisplayNumberFormat = "###,###";
 
         /// <summary>
         /// 並び順の表示文字列
         /// </summary>
-        public static Dictionary<int, string> SortSymbolDict { get; set; } = new()
+        public static readonly Dictionary<int, string> SortSymbolDict = new()
         {
             [1] = "▲",
             [2] = "▼",
